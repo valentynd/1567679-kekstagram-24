41752d0539b37ef7bf8f535a680e0c2077302b85
valentynd <valentin.diravka@gmail.com>
2021-10-20 00:21:29 +0300

    fix for linter

--- js/main.js
+++ js/main.js
@@ -1,19 +1,13 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
+function getRandomIntInclusive(min, max){
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 getRandomIntInclusive(9, 101);
 
-
-function checkLenghtString(strFirst, maxLenght) {
-
-    let stringLength=strFirst.length;
-
-    return (stringLength<=maxLenght)?true:false;
-
+function checkLenghtString(strFirst, maxLenght){
+  const stringLength=strFirst.length;
+  return stringLength<=maxLenght;
 }
-
-checkLenghtString("Hellow", 10);
+checkLenghtString('Hellow', 10);
